115b9a95556afd20b49d507d9f2fda7b1fc25eae
LuoGuangcong <guangcong.luo@dfymobile.com>
2015-08-04 11:18:31 +0800

    修改main页面

--- app/js/main.js
+++ app/js/main.js
@@ -7,6 +7,11 @@
 
   var app = angular.module("myApp",["ngRoute","ngAnimate","ngResource"]);
 
+  app.run(['$rootScope',function($rootScope){
+    $rootScope.navShow = false;
+    $rootScope.mainMove = false;
+  }]);
+
 
   app.config(["$routeProvider",function($routeProvider){
   	$routeProvider
@@ -16,21 +21,22 @@
   		})
   		.otherwise({
         	// redirectTo : '/main'
-        });
+      });
   }]);
 
   app.controller("mainController",['$scope',function($scope){
 
   }]);
 
-  app.controller("homeController",['$scope',function($scope){
+  app.controller("homeController",['$scope','$rootScope',function($scope,$rootScope){
   	$scope.text = 'my first controller';
   	$scope.menuBtnCls = {
-  		initCls:true,
   		actCls:false
   	};
   	$scope.showNav = function(){
   		$scope.menuBtnCls.actCls = !$scope.menuBtnCls.actCls;
+      $rootScope.navShow = !$rootScope.navShow;
+      $rootScope.mainMove = !$rootScope.mainMove;
   	};
   }]);
 
